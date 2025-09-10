bb8ce07695bf227d12e5db776c156e3ab8255cf6
MarsyaAmalia <marsyaamalia505@gmail.com>
2025-09-10 10:05:37 +0800

    Day3_Komentar.java

---
+++ Day3_Komentar.java
@@ -0,0 +1,15 @@
+public class Day3 {
+    public static void main(String[] args) {
+      Byte kkm = 60;
+      System.out.println("Nilai kkm saya: "+kkm);
+      
+      if (kkm >75) {
+        System.out.printf("Selamat!! Nilai anda tuntas%n");
+        // Komentar : ketika inputan terpenuhi >75 maka pada if akan menampilkan nilai tuntas
+      }else{
+        System.out.printf("Maaf, Anda remedial%nsemangat!!");
+        //Komentar : namun jika inputan tidak memenuhi <75 maka akan diproses oleh else sebagai tidak tuntas
+      }
+      
+  }
+}
