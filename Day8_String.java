52123b861d4a3721032ce3cb57b3b225491ae215
MarsyaAmalia <marsyaamalia505@gmail.com>
2025-09-15 03:40:08 +0800

    Day8_String.java

---
+++ Day8_String.java
@@ -0,0 +1,19 @@
+public class day8{
+    public static void main(String[] args) {
+String m = "semoga saya lolos challange 100daysofcoding 2025";
+String n = "dan bisa menjadi bagian dari infomatics study club";
+String h = "#untuk menggabungkan 2 string pada sout diberi tanda +#";
+String u = "#untuk mengetahui berapa banyak karakter pada deklarasi string kita menggunakan length#";
+
+System.out.printf("\n%s %s\n",m,n);
+System.out.println(h);
+//contoh
+System.out.println("CONTOH: " + m + n);
+System.out.println(u);
+//contoh
+System.out.println("CONTOH: " + m.length() +","+ u.length());
+// pada variable m terdapat 48 karakter
+// pada variable u terdapat 87 karakter
+      
+  }
+}
