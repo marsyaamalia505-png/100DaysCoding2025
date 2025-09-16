967630935c5966d9ed35e4ad6a86f1a745b833ec
MarsyaAmalia <marsyaamalia505@gmail.com>
2025-09-16 09:09:57 +0800

    Day9_DekInsiUpdate.java

---
+++ Day9_DekInsiUpdate.java
@@ -0,0 +1,20 @@
+public class day9{
+    public static void main(String[] args) {
+     double marsya,sasi ;
+     char sisi, abel;
+     // bentuk deklarasi 
+     marsya = 18.3;
+     sasi = 2.4;
+     sisi = 'K';
+     abel = 'U';
+     // bentuk inisialisasi
+     System.out.printf("\n%.1f \n%.1f \n%c \n%c\n",marsya,sasi,sisi,abel);
+     
+     double mrsyiie= 17.6; // bentuk deklarasi + inisialisasi
+     System.out.println("sebelum update: "+mrsyiie);
+     mrsyiie = 189.8;
+     System.out.println("setelah update: "+mrsyiie);
+     // bentuk update variabel
+    
+  }
+}
