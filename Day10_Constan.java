d3a41e75d8d3745f028b649804d0d3da32bc2c4a
MarsyaAmalia <marsyaamalia505@gmail.com>
2025-09-17 17:30:43 +0800

    Day10_Constan.java

---
+++ Day10_Constan.java
@@ -0,0 +1,12 @@
+public class day10{
+    public static void main(String[] args) {
+     double mrsyiie= 17.6; 
+     System.out.println("sebelum update: "+mrsyiie);
+     mrsyiie = 189.8;
+     System.out.println("setelah update: "+mrsyiie);
+     final double mrsyi= 189.8;
+     System.out.println("final varaibel: "+mrsyi);
+     // setelah vatiabel di finalkan maka nilainya sudah tidak bisa di inisalisasi lagi
+    
+  }
+}
