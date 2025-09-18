a417344acaa178fc6e0b60e5b5420aea01a19f7f
MarsyaAmalia <marsyaamalia505@gmail.com>
2025-09-19 03:46:38 +0800

    Day12_LatihanBio.java

---
+++ Day12_LatihanBio.java
@@ -0,0 +1,18 @@
+public class Day12 {
+    public static void main(String[] args) {
+      System.out.println("\thalo ini bidata saya: ");
+String nama,alamat,acc ;
+nama = "Marsya Amalia";
+alamat = "Padzang-padzang";
+acc= "mrsyiiee_";
+int  date,year;
+date = 15;
+year = 2007;
+float berat,tinggi;
+berat = 42.2f;
+tinggi = 152.1f;
+
+System.out.printf("Haw there my name is %s i,m offially living at %s \nmy birth on %d mei %d  i also  have an ig acc %s \nand last point my weigth & heigth is %.1f kg - %.1f cm ",nama,alamat,date,year,acc,berat,tinggi);
+
+  }
+}
