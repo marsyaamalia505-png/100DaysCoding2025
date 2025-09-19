33dba546bfc197de675cc6f4143c273ecdd3c3fe
MarsyaAmalia <marsyaamalia505@gmail.com>
2025-09-19 13:06:12 +0800

    Day12_LatihanBio.java

--- Day12_LatihanBio.java
+++ Day12_LatihanBio.java
@@ -1,18 +1,31 @@
+import java.util.Scanner;
+
 public class Day12 {
     public static void main(String[] args) {
-      System.out.println("\thalo ini bidata saya: ");
-String nama,alamat,acc ;
-nama = "Marsya Amalia";
-alamat = "Padzang-padzang";
-acc= "mrsyiiee_";
-int  date,year;
-date = 15;
-year = 2007;
-float berat,tinggi;
-berat = 42.2f;
-tinggi = 152.1f;
-
-System.out.printf("Haw there my name is %s i,m offially living at %s \nmy birth on %d mei %d  i also  have an ig acc %s \nand last point my weigth & heigth is %.1f kg - %.1f cm ",nama,alamat,date,year,acc,berat,tinggi);
-
-  }
+  Scanner isc = new Scanner (System.in);
+  System.out.println("\t>>Selamat Datang Beranda Infomasi Berbasis Inputan<<\n\nSilahkan Masukkan Data Diri: ");
+  String nama, alamat, acc,tempat;
+    nama =isc.nextLine();
+    alamat = isc.nextLine();
+    acc =isc.nextLine();
+    tempat =isc.nextLine();
+  int umur = isc.nextInt();
+  int tanggal,tahun;
+    tanggal= isc.nextInt();
+    tahun = isc.nextInt();
+  double tb, bb;
+    tb = isc.nextDouble();
+    bb = isc.nextDouble();
+  char goldar = isc.next().charAt(0);
+  boolean data= isc.nextBoolean();
+  
+  System.out.printf("Nama: %s \nUmur: %d \nTTL: %s, %d Mei %d \nAlamat: %s  \nSosmed: %s \nGolDar: %c \nTb/Bb: %.1f/%.1f \nData yang anda masukkan: %b \n",nama,umur,tempat,tanggal,tahun,alamat,acc,goldar,tb,bb,data);
+  System.out.print("\n\t\t>>Terimakasih atas infomasi anda<< \nJika ada data yang ingin diubah silahkan klik dibawah ini: \n\n");
+  tb = isc.nextDouble();
+  System.out.println("Setelah update tb: "+tb);
+  final double tinggi= isc.nextDouble();
+  System.out.println("Infomasi fiks tb: "+tinggi);
+  
+  
+    }
 }
