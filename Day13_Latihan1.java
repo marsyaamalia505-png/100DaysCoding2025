71f7a2ca7639f767ae00a1efffda97c445f25041
MarsyaAmalia <marsyaamalia505@gmail.com>
2025-09-20 10:29:39 +0800

    Day13_Latihan1.java

---
+++ Day13_Latihan1.java
@@ -0,0 +1,86 @@
+public class Main {
+    public static void main(String[] args) {
+    System.out.println("variable sebelum update");
+    String nama, pendek;
+    nama = "marsya";
+    pendek= "sya";
+    byte si, yi;
+    si = 10;
+    yi = 20;
+    short ma, pa;
+    ma = 200;
+    pa = 100;
+    int la, qa;
+    la = 1000;
+    qa = 2000;
+    long ru,da;
+    ru = 10000;
+    da = 20000;
+    char le, cu;
+    le = 'A';
+    cu = 'B';
+    float re, ge;
+    re = 10.1f;
+    ge = 20.2f;
+    double ii, is;
+    ii = 20.2;
+    is = 10.1;
+    boolean aku, sisi;
+    aku = true;
+    sisi= true;
+    
+    System.out.println(nama);
+    System.out.println(pendek);
+    System.out.println(si);
+    System.out.println(yi);  
+    System.out.println(ma);
+    System.out.println(pa);
+    
+    System.out.print(la+"\n");
+    System.out.print(qa+"\n");
+    System.out.print(ru+"\n");
+    System.out.print(da+"\n");
+    System.out.print(le+"\n");
+    System.out.print(cu+"\n");
+    
+    System.out.printf("re: %.1f%n ge: %.1f%n ii: %.1f%n is: %.1f%n aku: %b%n sisi: %b%n%n",re,ge,ii,is,aku,sisi);
+      
+    System.out.println("sesudah update variabel");
+    
+    nama = "amalia";
+    pendek= "yie";
+    si = 20;
+    yi = 30;
+    ma = 300;
+    pa = 400;
+    la = 2000;
+    qa = 3000;
+    ru = 30000;
+    da = 40000;
+    le = 'C';
+    cu = 'D';
+    re = 40.1f;
+    ge = 50.2f;
+    ii = 90.2;
+    is = 80.1;
+    aku = false;
+    sisi= false;
+    
+    System.out.println(nama);
+    System.out.println(pendek);
+    System.out.println(si);
+    System.out.println(yi);  
+    System.out.println(ma);
+    System.out.println(pa);
+    
+    System.out.print(la+"\n");
+    System.out.print(qa+"\n");
+    System.out.print(ru+"\n");
+    System.out.print(da+"\n");
+    System.out.print(le+"\n");
+    System.out.print(cu+"\n");
+    
+    System.out.printf("re: %.1f%n ge: %.1f%n ii: %.1f%n is: %.1f%n aku: %b%n sisi: %b%n",re,ge,ii,is,aku,sisi);
+        
+  }
+}
