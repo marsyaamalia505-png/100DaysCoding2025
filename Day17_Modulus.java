0cc453d0878fe6de5005d461d7498c89f6841ec0
MarsyaAmalia <marsyaamalia505@gmail.com>
2025-09-24 14:13:45 +0800

    Day17_Modulus.java

---
+++ Day17_Modulus.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Day15{
+  public static void main (String[] args) {
+  
+  Scanner isc = new Scanner (System.in);
+  String nilai = isc.nextLine();
+  int string = Integer.parseInt(nilai);
+    System.out.print("Nilai Inputan\t: "+ string + "\n");
+  int sisa, bagi;
+    bagi = string / 2;
+    System.out.print("Hasil dibagi 2\t: "+bagi+"\n");
+    sisa = string % 2;
+  System.out.println("\tJIKA GENAP HASILNYA (0) dan GANJIL HASILNYA (1)");
+  System.out.print("Hasil Sisa Bagi\t: "+sisa);
+    
+  }
+} 
