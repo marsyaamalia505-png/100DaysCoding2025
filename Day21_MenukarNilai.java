06b4a832331087cbe7a15cafb35da2c6a5945705
MarsyaAmalia <marsyaamalia505@gmail.com>
2025-09-28 16:30:30 +0800

    Day21_MenukarNilai.java

--- Day21_MenukarNilai.java
+++ Day21_MenukarNilai.java
@@ -9,7 +9,7 @@ public class Day21 {
       long B= Long.parseLong(angka1);// contoh: 15
       System.out.printf("A: %d%nB: %d%n",A,B);
       
-      A+=B;  // A= 12+15 >> 27 (nilai varibel A ter-update dari 12 jadi 27)
+      A+=B; // A= 12+15 >> 27 (nilai varibel A ter-update dari 12 jadi 27)
       B=A-B; // B = 27-15 >> 12 
       A-=B; // A= 27- 12 >> 15 
       System.out.println("Nilai A sekarang: "+A);
