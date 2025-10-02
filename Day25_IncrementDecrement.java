87a01bfcef65651e9b62316d02b5afd8b590bd64
MarsyaAmalia <marsyaamalia505@gmail.com>
2025-10-02 21:51:08 +0800

    Day25_IncrementDecrement.java

--- Day25_IncrementDecrement.java
+++ Day25_IncrementDecrement.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Day24 {
+public class Day25 {
     public static void main(String[] args) {
       Scanner isc = new Scanner (System.in);
       System.out.printf("OPERASI INCREMENT(JUMLAH 1)");
