1dbf83a4b1058d56b26306332d95850a5514691f
MarsyaAmalia <marsyaamalia505@gmail.com>
2025-10-06 13:38:01 +0800

    Day29_OperatorLebihKecil_dan_Besar.java

--- Day29_OperatorLebihKecil_dan_Besar.java
+++ Day29_OperatorLebihKecil_dan_Besar.java
@@ -1,20 +1,22 @@
- import java.util.Scanner;
+import java.util.Scanner;
 
 public class Day29 {
     public static void main(String[] args) {
         Scanner isc = new Scanner (System.in);
-   System.out.println("\tOPERATOR PERBANDINGAN LEBIH KECIL(<) DAN LEBIH BESAR (>)");
-   System.out.print("\n\t====Sistem Pengecekan Syarat Masuk Wahana Roller Coaster====");
-   System.out.print("\nJika umur anda <17 tahun dan tinggi anda <140cm MAKA ANDA TIDAK BOLEH NAIK.\nTapi jika umur anda >17 tahun Namun tinggi anda <140 cm ANDA HARUS DUDUK DITENGAH\n");
-    System.out.print("\nMasukkan Umur: \n");
-     int umur= isc.nextInt();
-    System.out.print("Masukkan Tinggi: \n");
-    double tinggi = isc.nextDouble();
- boolean syaratUmur= umur >17;
- System.out.println("Umur anda ("+syaratUmur+")");
- boolean syaratTinggi= tinggi > 1.4;// jika false anda tidak boleh masuk
- System.out.println("Tinggi anda ("+syaratTinggi+")");// tapi jika ini false maka boleh masuk namun harus duduk di tengah
-
+       int PermenMarsya= isc.nextInt(), PermenKakak= isc.nextInt();
+       int permenKakek= 50;
+           PermenMarsya+= PermenMarsya*50.0/100;
+           PermenKakak+= PermenKakak*20.0/100;
+            PermenMarsya-=10;
+             PermenKakak-=3;
+  
+        boolean bandingkan= PermenMarsya > PermenKakak;
+          System.out.println("Apakah permen marsya lebih banyak dari permen kakak: "+bandingkan);
+              PermenMarsya+=PermenKakak;
+              System.out.println("Hasil jumlah Permen Marsya dan kakak "+PermenMarsya);
+        boolean bandingkan2= PermenMarsya<permenKakek;;
+            System.out.println("Apakah permen marsya + kakak lebih sedikit dari permen kakek: "+bandingkan2);
+ 
 
     }
-} 
+}
