af92eac260d439cc79af927385b78d0be5807962
MarsyaAmalia <marsyaamalia505@gmail.com>
2025-11-26 19:07:13 +0800

    Add Day800_Array1Dimensi.java with array examples

---
+++ Day800_Array1Dimensi.java
@@ -0,0 +1,54 @@
+
+package Marsya;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class Day80 {
+    public static void main(String[] args) {
+        Scanner isc = new Scanner(System.in);
+                
+        byte[] Byte={
+          10, 20, 30, 40, 50  
+        };
+         System.out.println("Array byte\t: "+ Arrays.toString(Byte));     
+        short [] Short = {
+          1000, 2000, 3000, 4000, 5000  
+        };
+         System.out.println("Array short\t: "+ Arrays.toString(Short));     
+        int [] Int = {
+          1,2,3,4,5  
+        };
+         System.out.println("Array int\t: "+ Arrays.toString(Int));     
+        long []Long={
+            10000,20000,30000,40000,50000
+        };
+         System.out.println("Array long\t: "+ Arrays.toString(Long));     
+        float []Float={
+         1.1f, 2.2f, 3.3f, 4.4f, 5.5f  
+        };
+         System.out.println("Array float\t: "+ Arrays.toString(Float));     
+         double []Double={
+          10.5, 20.5, 30.5, 40.5, 50.5  
+        };
+         System.out.println("Array double\t: "+ Arrays.toString(Double));     
+        
+        char [] Char={
+          'L', 'U','L', 'U','S' 
+        };
+         System.out.println("Array char\t: "+ Arrays.toString(Char));     
+        String [] string = {
+            "smg", "lulus","mata", "kulyah","ddp"
+        };
+         System.out.println("Array string\t: "+ Arrays.toString(string));     
+        
+        
+        
+        
+        
+        
+    
+    }
+    
+}
