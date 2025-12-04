5b83ae6abe213099853739e71f777b4438cdc9a3
MarsyaAmalia <marsyaamalia505@gmail.com>
2025-12-04 20:03:33 +0800

    Add Day88_MengaksesElemenDenganForEach.java

---
+++ Day88_MengaksesElemenDenganForEach.java
@@ -0,0 +1,20 @@
+
+package Marsya;
+
+import java.util.Scanner;
+
+public class Day88 {
+    public static void main(String[] args) {
+        Scanner isc = new Scanner(System.in);
+        int [] angka = {
+          50,60,70,80,90,100  
+        };
+        
+        for(int i:angka){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+        
+    }
+    
+}
